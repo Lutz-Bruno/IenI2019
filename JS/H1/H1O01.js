b211fabb0c91ad1a68fc54d1a13d6238ca0a9245
Lutz-Bruno <151237@csgaugustinus.nl>
2019-11-20 10:19:20 +0000

    beginsel

--- JS/H1/H1O01.js
+++ JS/H1/H1O01.js
@@ -1,17 +1,23 @@
 function setup() {
-  canvas = createCanvas(1000,500);
-  background('orange');
+  canvas = createCanvas(450,450);
+  background('grey');
   canvas.parent('processing');
   noLoop();
 }
 
 function draw() {
-  // groene cirkel zonder rand
   
   noStroke();
   fill('green');
-  ellipse(300,200,400);
-  
+  ellipse(225,225,400);
+
+  fill('white');
+  ellipse(225,225,300)
+
+  stroke('green');
+  fill('white');
+  strokeWeight(10);
+  rect(125,125,200,200)
   // witte rechthoek met rode rand
   
   stroke('red');
